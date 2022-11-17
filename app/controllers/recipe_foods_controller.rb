aee4bd08175580d016412b6b3ff1d082f81f2770
unknown <aquinasmoriasi@gmail.com>
2022-11-17 10:42:39 +0300

    correct errors

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,25 +1,25 @@
 class RecipeFoodsController < ApplicationController
   def new
+    @recipe_food = RecipeFood.new
+    @foods = current_user.foods
     recipe = Recipe.find(params[:recipe_id])
     unless recipe.user == current_user
       flash[:alert] =
         'You do not have access to add an ingredient on a recipe that does not belong to you.'
       return redirect_to recipes_path
     end
-
-    @recipe_food = RecipeFood.new
-    @foods = current_user.foods
   end
 
-  def create
-    @recipe = Recipe.find(params[:recipe_id])
-    recipe_food = RecipeFood.new(recipe_food_params)
-    recipe_food.recipe = @recipe
+ def create
+    recipe_food = RecipeFood.create(recipe_food_params)
+    recipe_food.recipe = Recipe.find(params[:recipe_id])
+    recipe_food.food = Food.find(params[:food])
 
     if recipe_food.save
-      redirect_to recipe_path(@recipe), notice: 'New ingredient was successfully added.'
+      flash[:success] = 'New Ingredient was successfully added.'
+      redirect_to recipe_path(recipe_food.recipe)
     else
-      flash[:alert] = 'New Ingredient adding Failed. Please try again.'
+      flash[:danger] = 'New Ingredient adding Failed. Please try again.'
     end
   end
 
